c93305a94208c3fb648f6260dcd6206703a22a80
Yan C Lin <yonalin1@gmail.com>
2019-05-20 18:11:28 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 
 class GifListContainer extends Component {
-  state = { comments: [] };
 
-  componentDidMount() {
-  fetch('http://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g')
-    .then(response => response.json())
-    .then(data => {
-    this.setState({ comments: comments
-      })
-    })
-  }
-
-   submitHandler() => {
-
+   submitHandler= event => {
+    fetch('http://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g')
+   .then(res => res.json())
+   .then(json => this.setState({data: json}))
    }
   render() {
       return (
         <div>
-          <GifSearch gifs={this.props.submitHandler} />
+          <button onClick={this.props.submitHandler}>Click to Fetch!</button>
         </div>
       )
     }
