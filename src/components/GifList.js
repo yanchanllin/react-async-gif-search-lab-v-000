523f16526696c2c6fbd252d6f9f76210ac76d1b6
Yan C Lin <yonalin1@gmail.com>
2019-05-20 19:12:39 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 
 class GifList extends Component {
 
-
+each.gif 
 
   render() {
       return (
         <div>
-
+        <h3>gifs</h3>
+       <ul>
+        {this.props.gifs.map(gif =>
+       <li key={gif.id}><img src={gif.images.original.url} alt= 'gif list'/> </li> )}
+        </ul>
         </div>
       )
     }
