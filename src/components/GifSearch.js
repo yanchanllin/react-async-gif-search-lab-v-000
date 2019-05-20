2aa69748bfc4242d77355d566eb8308f097c705b
Yan C Lin <yonalin1@gmail.com>
2019-05-20 18:25:46 +0000

    Automatically backed up by Learn

---
+++ src/components/GifSearch.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class GifSearch extends Component {
+
+each.gif 
+
+  render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+        <input type="text"  value={this.state.username} />
+        
+      </form>
+      )
+    }
+
+
+}
+
+export default GifSearch
