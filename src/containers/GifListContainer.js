a8055b43960e697758907480b5c1853c70673fe1
Yan C Lin <yonalin1@gmail.com>
2019-05-20 18:03:40 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
 class GifListContainer extends Component {
+  state = { comments: [] };
+  
+  componentDidMount() {
+  fetch('http://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g')
+    .then(response => response.json())
+    .then(data => {
+    this.setState({ comments: comments
+      })
+    })
+  }
 
+   submitHandler() => {
+     
+   }
   render() {
       return (
         <div>
-
+          <GifSearch this.submitHandler.props />
         </div>
       )
     }
+    
 
 }
 
