19c101d1dc48ba011a1442b9d84bd51fb9d0f755
Yan C Lin <yonalin1@gmail.com>
2019-05-20 18:20:39 +0000

    Automatically backed up by Learn

---
+++ src/components/GifList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class GifList extends Component {
+
+  
+
+  render() {
+      return (
+        <div>
+      
+        </div>
+      )
+    }
+
+
+}
+
+export default GifList 
